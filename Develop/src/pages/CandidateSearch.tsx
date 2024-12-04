a8c0ca54dfde7d48b84f9f1935182318ce417735
denisetruuong <denise.truong1124@gmail.com>
2024-12-04 09:49:18 -0600

    candidate interface and search

---
+++ Develop/src/pages/CandidateSearch.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import { searchGithub, searchGithubUser } from '../api/API';
+import { Candidate } from '../interfaces/Candidate.interface';
+import CandidateCard from '../components/CandidateCard';
+
+const CandidateSearch = () => {
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
+  const [currentCandidate, setCurrentCandidate] = useState<Candidate | null>(null);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await searchGithub();
+      setCandidates(data);
+      const userData = await searchGithubUser(data[currentIndex].login);
+      setCurrentCandidate(userData);
+    };
+    fetchData();
+  }, []);
+
+  const handleNext = async (save:boolean) => {
+    if (save) {
+      const currentCandidates = JSON.parse(localStorage.getItem('savedCandidates') || '[]');
+      currentCandidates.push(currentCandidate);
+      localStorage.setItem('savedCandidates', JSON.stringify(currentCandidates));
+
+    }
+    if (currentIndex < candidates.length -1) {
+    const newIndex = currentIndex + 1;
+    setCurrentIndex(newIndex);
+    const login = candidates[newIndex]?.login || '';
+    const userData = await searchGithubUser(login);
+    setCurrentCandidate(userData);
+    } else {
+      setCurrentIndex(0);
+      const data = await searchGithub();
+      setCandidates(data);
+      const userData = await searchGithubUser(data[currentIndex].login);
+      setCurrentCandidate(userData);
+
+  };
+}
+
+  return (
+    <>
+    <h1>Candidate Search</h1>
+   
+
+    </> 
+  )
+};
+
+export default CandidateSearch;
