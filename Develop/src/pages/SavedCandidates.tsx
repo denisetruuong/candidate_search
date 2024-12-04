b3720b768cce82ad4320bfaafeb8aad5b240dff3
denisetruuong <denise.truong1124@gmail.com>
2024-12-04 10:02:08 -0600

    saved candidates

--- Develop/src/pages/SavedCandidates.tsx
+++ Develop/src/pages/SavedCandidates.tsx
@@ -1,7 +1,58 @@
+import { Candidate } from "../interfaces/Candidate.interface";
+
 const SavedCandidates = () => {
+  const candidates: Candidate[] = []; // Add an array of candidates and define its type
+
   return (
-    <>
-      <h1>Potential Candidates</h1>
+    <div> {/* Wrap the JSX elements in a parent element */}
+      <h1>Possible Candidates</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Email</th>
+            <th>Company</th>
+            <th>Location</th> {/* Fix the closing tag for 'th' */}
+            <th>Bio</th>
+          </tr>
+        </thead>
+        <tbody>
+        {JSON.parse(localStorage.getItem("savedCandidates") ?? "[]")?.length === 0 ? (
+            <tr>
+              <td colSpan={7}>No candidates saved</td>
+            </tr>
+          ) : (
+            JSON.parse(localStorage.getItem("savedCandidates") || "[]").map((candidate: Candidate, index:number) => (
+              <tr key={index}>
+                <td>
+                  <img src={candidate.avatar_url} alt="avatar" />
+                </td>
+                <td>
+                  <a src={candidate.html_url}>{candidate.login}</a>
+                </td>
+                <td>{candidate.location}</td>
+                <td>{candidate.email}</td>
+                <td>{candidate.company}</td>
+                <td>{candidate.bio}</td>
+                <td>
+                  <button className="btnX"
+                    onClick={() => {
+                      const savedCandidates = JSON.parse(localStorage.getItem("savedCandidates") || "[]");
+                      const newCandidates = savedCandidates.filter((c: Candidate) => c.login !== candidate.login);
+                      localStorage.setItem("savedCandidates", JSON.stringify(newCandidates));
+                      window.location.reload();
+                    }}
+                  >
+                    X
+                  </button>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
     </>
   );
 };
